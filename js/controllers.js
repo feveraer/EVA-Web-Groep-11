723904c0a536b09f6d627247db59bbf68e831d2a
Thomas Vansevenant <thomas.vansevenant@hotmail.com>
2015-10-16 11:20:03 +0200

    Start Refactoring

--- js/controllers.js
+++ js/controllers.js
@@ -2,9 +2,15 @@
 var challengeControllers = angular.module('challengeControllers', []);
 
 challengeControllers.controller('challengeDetailCtrl', ['$scope', 'Challenge', function($scope, Challenge) {
-    var challenges = Challenge.query();
-    $scope.task = challenges;
+    var tasks = Challenge.query();
+    $scope.task = tasks;
+
+
+    $scope.firstTask = task[0];
 
     console.log(challenges);
 
+    $scope.daysLeft = function (date) {
+
+    }
 }]);
--- js/services.js
+++ js/services.js
@@ -6,4 +6,5 @@ challengeServices.factory('Challenge', ['$resource',
         return $resource("http://95.85.59.29:1337/api/users/5620a54429b03cf71b0607e9/tasks", {}, {
             query: {method:'GET', isArray:true}
         });
-    }]);
+    }
+]);
