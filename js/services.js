6d6bc6ef944213842c78e8dee8c6baede8e7001a
Tyslan <verschingellorenz@hotmail.com>
2015-10-24 15:42:57 +0200

    refactor apiBaseUrl

--- js/services.js
+++ js/services.js
@@ -1,10 +1,10 @@
-
 var challengeServices = angular.module('challengeServices', ['ngResource']);
+var apiUrl = "http://95.85.59.29:1337/api/"
 
 challengeServices.factory('Challenge', ['$resource',
-    function($resource){
-        return $resource("http://95.85.59.29:1337/api/users/56224ab96dcac34e5e596a35/tasks", {}, {
-            query: {method:'GET', isArray:true}
+    function ($resource) {
+        return $resource(apiUrl + "users/56224ab96dcac34e5e596a35/tasks", {}, {
+            query: {method: 'GET', isArray: true}
         });
     }
 ]);
