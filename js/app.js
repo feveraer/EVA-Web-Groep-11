950e0cc2a5da2e152b75fa5eb8500f29a98808d0
Thomas Vansevenant <thomas.vansevenant@hotmail.com>
2015-10-27 19:34:32 +0100

    Refactoring challengeController

--- js/app.js
+++ js/app.js
@@ -1,44 +1,21 @@
-
-var app = angular.module('EvaWeb',['ui.router', 'ngRoute', 'challengeControllers', 'challengeServices', 'ngMaterial', 'angular-timeline']);
+var app = angular.module('EvaWeb', ['ui.router', 'ngRoute', 'app.challenge', 'challengeServices', 'ngMaterial', 'angular-timeline']);
 
 
 app.config([
     '$stateProvider',
     '$urlRouterProvider',
-    function($stateProvider, $urlRouterProvider) {
+    function ($stateProvider, $urlRouterProvider) {
         $stateProvider
             .state('home', {
                 url: '/home',
                 templateUrl: './views/home.html',
-                controller: 'challengeDetailCtrl'
+                controller: 'ChallengeController',
+                controllerAs: 'vmChallenge'
             });
 
         $urlRouterProvider.otherwise('/home');
     }
 ]);
-
-
-
-app.directive('leafRating', function () {
-    return {
-        restrict: 'A',
-        template:
-        '<div class="leaf" ng-repeat="leaf in leafs" ng-class="leaf"></div>' ,
-        scope: {
-            ratingValue: '=',
-            max: '='
-        },
-        link: function (scope, elem, attrs) {
-            scope.leafs = [];
-            for (var i = 0; i < scope.max; i++) {
-                scope.leafs.push({
-                    filled: i < scope.ratingValue
-                });
-            }
-        }
-    }
-});
-
 
 
 
---
+++ js/challenge.js
@@ -0,0 +1,107 @@
+angular
+    .module('app.challenge', ['ui.router', 'ngRoute', 'ngMaterial', 'angular-timeline'])
+    .controller('ChallengeController', ChallengeController)
+    .controller('DialogController', DialogController)
+    .directive('leafRating', leafRating);
+
+ChallengeController.$inject = ['$mdDialog', 'Challenge']
+DialogController.$inject = ['$mdDialog', 'vmChallenge']
+
+function ChallengeController($mdDialog, Challenge) {
+    var vmChallenge = this;
+
+    Challenge.query().$promise.then(function (data) {
+
+        var tasks = data;
+        var currentTask = tasks[3];
+        var completedTasks = [];
+
+        vmChallenge.ratings = [{
+            current: currentTask.challenge.difficulty,
+            max: 3
+        }];
+
+        //If testing use dummyTasks instead of tasks
+        tasks.forEach(function (task) {
+            if (task.completed) {
+                completedTasks.push(task)
+            }
+        });
+
+        completedTasks.sort(sortTasksByDateDesc);
+
+        vmChallenge.tasks = completedTasks;
+
+        vmChallenge.dueDate = currentTask.dueDate;
+        vmChallenge.completed = currentTask.completed;
+        vmChallenge.challenge = currentTask.challenge;
+    });
+
+
+    vmChallenge.showAdvanced = function (ev) {
+        console.log("show");
+        $mdDialog.show({
+            controller: DialogController,
+            templateUrl: './views/challengeDialog.html',
+            parent: angular.element(document.body),
+            targetEvent: ev,
+            clickOutsideToClose: true,
+            locals: {
+                data: vmChallenge.challenge
+            }
+        });
+    };
+
+    vmChallenge.daysLeft = dDiff(Date.now());
+
+}
+
+function dDiff(dueDate) {
+    var magicNumber = (1000 * 60 * 60 * 24);
+    var dayDiff = Math.floor((dueDate - Date.now()) / magicNumber);
+    if (angular.isNumber(dayDiff)) {
+        return dayDiff + 1;
+    }
+}
+
+function DialogController(vmChallenge, $mdDialog, data) {
+    console.log("dia")
+    vmChallenge.mdDialogData = data;
+
+    vmChallenge.hide = function () {
+        $mdDialog.hide();
+    };
+
+    vmChallenge.cancel = function () {
+        $mdDialog.cancel();
+    };
+
+    vmChallenge.answer = function (answer) {
+        $mdDialog.hide(answer);
+    };
+}
+
+var sortTasksByDateDesc = function (task1, task2) {
+    return new Date(task2.dueDate) - new Date(task1.dueDate);
+};
+
+//Directive
+function leafRating() {
+    return {
+        restrict: 'A',
+        template: '<div class="leaf" ng-repeat="leaf in leafs" ng-class="leaf"></div>',
+        scope: {
+            ratingValue: '=',
+            max: '='
+        },
+        link: function (scope, elem, attrs) {
+            scope.leafs = [];
+            for (var i = 0; i < scope.max; i++) {
+                scope.leafs.push({
+                    filled: i < scope.ratingValue
+                });
+            }
+        }
+
+    }
+}
