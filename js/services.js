8890d1862d2efdb52f77740e1b02d810321cd765
Tyslan <verschingellorenz@hotmail.com>
2015-10-27 17:29:47 +0100

    user id aangepast naar api

--- js/services.js
+++ js/services.js
@@ -3,7 +3,7 @@ var apiUrl = "http://95.85.59.29:1337/api/"
 
 challengeServices.factory('Challenge', ['$resource',
     function ($resource) {
-        return $resource(apiUrl + "users/562e08591349e7816d10dbd4/tasks", {}, {
+        return $resource(apiUrl + "users/562f3f87b0b8dc041bcc6ba7/tasks", {}, {
             query: {method: 'GET', isArray: true}
         });
     }
