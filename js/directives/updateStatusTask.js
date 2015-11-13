ea36c57b77b9c2e21709287f544ba40d85bcef3b
Tyslan <verschingellorenz@hotmail.com>
2015-11-13 07:55:26 +0100

    Add more comments to directive

--- js/directives/updateStatusTask.js
+++ js/directives/updateStatusTask.js
@@ -11,19 +11,22 @@
  */
 function increaseStatusTask(ApiCallService, $location) {
     return {
+        // use directive as attribute
         restrict: 'A',
         scope: {
+            // get element as link
             taskId: '@',
             taskStatus: '@'
         },
         link: function (scope, elem, attr) {
+            // Use $observe when you need to observe/watch a DOM attribute that contains interpolation (i.e., {{}}'s).
             attr.$observe('taskId', function (taskId) {
                 scope.taskId = taskId;
             });
             attr.$observe('taskStatus', function (taskStatus) {
                 scope.taskStatus = taskStatus;
             });
-
+            //click event
             elem.bind('click', function () {
                 var data = {"status": scope.taskStatus + 1};
                 if (scope.taskStatus === "0") {
